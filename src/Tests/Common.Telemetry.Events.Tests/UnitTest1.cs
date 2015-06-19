515080c94a174d51c45455484a75c746f2aef640
djfr <davidferreirarodrigues@gmail.com>
2015-06-19 13:43:10 +0100

    Added two blank test projects to the solution. Signed all the relevant assemblies.

---
+++ src/Tests/Common.Telemetry.Events.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Common.Telemetry.Events.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
